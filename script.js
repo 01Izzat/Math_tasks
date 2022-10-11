96d23c1821ba6f46ca3c5e7b79e7ad907fa44a86
izzatbekdev <Izzattexnika@gmail.com>
2022-10-11 21:41:21 +0500

    js

---
+++ script.js
@@ -0,0 +1,213 @@
+//MANTIQIY AMALLAR
+
+//--1
+// let z=+prompt('enter a whole number');
+// if(z>0) {
+//   console.log('a positive number');
+// }
+// else if(z<0) {
+//   console.log('a negative number');
+// }
+// else {
+//   console.log('letters and symbols cannot be entered');
+// }
+
+//---2 
+// const x=+prompt('enter a whole number');  //minus kiritganda just bervotti
+// if(x%2==1) {
+//   console.log('an odd number');
+// }
+// else if(x%2==0) {
+//   console.log('an even number');
+// }
+// else {
+//   console.log('letters and symbols cannot be entered');
+// }
+
+//---3
+// const y=prompt('enter a whole number');
+// if(y%2==0) {
+//   console.log('an even number');
+// }
+// else if(y%2==1) {
+//   console.log('an odd number');
+// }
+// else {
+//    console.log('letters and symbols cannot be entered');
+// }
+
+//---4
+
+//---5
+// const a=+prompt('enter a 1st number')
+// const b=+prompt('enter a 2nd number')
+// const c=+prompt('enter a 3rd number')
+
+// if(a>b && b>c) {
+//   console.log(true);
+// }
+// else if(a<b || b<c) {
+//   console.log('false');
+// }
+// else {
+//   console.log('letters and symbols cannot be entered');
+// }
+ 
+//---6
+// let a=+prompt('enter a 1st number')
+// let b=+prompt('enter a 2nd number')
+// let c=+prompt('enter a 3rd number')
+
+// if(a>b && b>c) {
+//   console.log('B lies between A and C');
+// }
+// else if(a<b || b<c) {
+//   console.log('B lies before A or after C');
+// }
+// else {
+//   console.log('letters and symbols cannot be entered');
+// }
+
+//---7
+// let a=+prompt('enter a 1st number')
+// let b=+prompt('enter a 2nd number')
+
+// if(a%2==1 && b%2==1) {
+//   console.log('A and B are odd number ');
+// }
+// else if (a%2==0 || b%2==0) {
+//   console.log('A or B is even number');
+// }
+// else {
+//   console.log('letters and symbols cannot be entered');
+// }
+
+//---8
+// let a=+prompt('enter a 1st number')
+// let b=+prompt('enter a 2nd number')
+// let c=+prompt('enter a 3rd number')
+// let d=+prompt('enter a 4th number')
+
+// if (a>0 && b>0 && c>0 && d>0) {
+//   console.log('all is positive numbers');
+// }
+// else if (a<0 || b<0 || c<0 || d<0) {
+//   console.log('a or b or c or d or all is negative number');
+// }
+// else {
+//   console.log('letters and symbols cannot be entered');
+// }
+
+// ---9
+// let a=+prompt('enter a 1st number')
+// let b=+prompt('enter a 2nd number')
+// let c=+prompt('enter a 3rd number')
+
+// if(a>0 && b>0) {
+//   console.log('2 of them positive');
+// }
+// else if(a>0 && c>0) {
+//   console.log('2 of them positive');
+// }
+// else if(b>0 && c>0) {
+//   console.log('2 of them positive');
+// }
+// else if((a<0, b<0)  || (b<0, c<0) || (a<0, c<0)) {
+//   console.log('2 of them negative');
+// }
+// else {
+//   console.log('a or b or c or d is negative number');
+// }
+
+//---10
+// let week=+prompt('enter the number of week');
+
+// if(week==1) {
+//   console.log('Monday');
+// }
+// else if(week==2) {
+//   console.log('Tuesday');
+// }
+// else if(week==3) {
+//   console.log('Wendesday');
+// }
+// else if(week==4) {
+//   console.log('Thursday');
+// }
+// else if(week==5) {
+//   console.log('Friday');
+// }
+// else if(week==6) {
+//   console.log('Suturday');
+// }
+// else if(week==7) {
+//   console.log('Sunday');
+// }
+// else {
+//   console.log('No such day per week  or  letters and symbols cannot be entered');
+// }
+
+//---11
+// let phoneNum=+prompt('enter phone number code')
+
+// if(phoneNum==90 || phoneNum==91) {
+//   console.log('Beeline');
+// }
+// else if(phoneNum==93 || phoneNum==94) {
+//   console.log('Ucell');
+// }
+// else if(phoneNum==95 || phoneNum==99) {
+//   console.log('Uzmobile');
+// }
+// else if(phoneNum==97 || phoneNum==88) {
+//   console.log('Mobiuz');
+// }
+// else if(phoneNum==98) {
+//   console.log('Perfectum');
+// }
+// else {
+//   console.log('no such network operator or letters and symbols cannot be entered');
+// }
+
+//---12
+// let counter=+prompt('enter a whole number')
+
+// if(counter>0) {
+//   console.log(Math.round(++counter));
+// }
+// else if(counter<0) {
+//   console.log(Math.round(--counter));
+// }
+// else {
+//   console.log('letters and symbols cannot be entered')
+// }
+
+//---13
+// let total=+prompt('enter a whole number')
+
+// if(total>0) {
+//   console.log(Math.round(total+3));
+// }
+// else if(total<0) {
+//   console.log(Math.round(total-2));
+// }
+// else {
+//   console.log('letters and symbols cannot be entered')
+// }
+
+//---14
+// let x=+prompt('x= enter a whole number')
+// let y=+prompt('y= enter a whole number')
+//   console.log(Math.round(Math.max(x,y)))
+
+//---15
+// let A11=+prompt("A11= enter a whole number")
+// let A21=+prompt("A21= enter a whole number")
+// let A31=+prompt("A31= enter a whole number")
+// console.log(Math.round(Math.max(A11, A21, A31)))
+
+//---16
+// let B11=+prompt("B11= enter a whole number")
+// let B21=+prompt("B21= enter a whole number")
+// let B31=+prompt("B31= enter a whole number")
+// console.log(Math.round(Math.min(B11, B21, B31)))
